5583bfcbe5a928a35c43b4b8f1676069c324d411
TalkingF <140305263+TalkingF@users.noreply.github.com>
2026-01-14 23:20:48 +1030

    add height based colour and tweak curve

--- terrain-testing/scripts/construct_terrain_3d_mesh.gd
+++ terrain-testing/scripts/construct_terrain_3d_mesh.gd
@@ -20,15 +20,18 @@ func _ready() -> void:
 	var elevation_map := elevation_gen.gen_elevation(WIDTH, LENGTH, false)
 	var moisture_map := moisture_gen.gen_moisture(WIDTH, LENGTH)
 	material_override = MESH_MATERIAL
+	material_override.vertex_color_use_as_albedo = true
 	gen_terrain(elevation_map, moisture_map)
 	
 	
 func gen_terrain(elevation_map: Array[PackedFloat32Array], moisture_map: Array[PackedFloat32Array]) -> void:
+	st.set_smooth_group(0)
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
 	for x in range(elevation_map.size() - 1):
 		for z in range(elevation_map[x].size() - 1):
 			var y := elevation_map[x][z]
-		
+			
 			var h00 := elevation_map[x][z] * 100.0
 			var h10 := elevation_map[x + 1][z] * 100.0
 			var h01 := elevation_map[x][z + 1] * 100.0
@@ -39,20 +42,32 @@ func gen_terrain(elevation_map: Array[PackedFloat32Array], moisture_map: Array[P
 			var v01 := Vector3(x, h01, z + 1)
 			var v11 := Vector3(x + 1, h11, z + 1)
 			
+			st.set_color(set_colour(h00))
 			st.add_vertex(v00)
+			st.set_color(set_colour(h10))
 			st.add_vertex(v10)
+			st.set_color(set_colour(h01))
 			st.add_vertex(v01)
 			
+			st.set_color(set_colour(h10))
 			st.add_vertex(v10)
+			st.set_color(set_colour(h11))
 			st.add_vertex(v11)
+			st.set_color(set_colour(h01))
 			st.add_vertex(v01)
 			
 	st.generate_normals()
 	mesh = st.commit()
 			
+func set_colour(height: float) -> Color:
+	height = height / 100.0
 
-			
-
+	if height < 0.3:
+		return Color.BLUE.lerp(Color.GREEN, height / 0.3)
+	elif height < 0.7:
+		return Color.GREEN.lerp(Color.SADDLE_BROWN, (height - 0.3) / 0.4)
+	else:
+		return Color.SADDLE_BROWN.lerp(Color.WHITE, (height - 0.7) / 0.3)
 
 	
 
